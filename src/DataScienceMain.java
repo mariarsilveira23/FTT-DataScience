bcc88423b3cf002f519d08d888b81d7d25d890f6
MARIA DE FATIMA ROCHA SILVEIR <081160042@cefsa.corp.local>
2018-08-27 20:36:36 -0300

    buffer

--- src/DataScienceMain.java
+++ src/DataScienceMain.java
@@ -1,5 +1,24 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class DataScienceMain {
 	public static void main(String[] args) {
+		File file = new File("Z:\\LPII\\.csv");
+		
+		try {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		String registro;
+		
+		while((registro = br.readLine()) != null) {
+			System.out.println(registro);
+		}
+		}
+		catch(Exception e){
+			
+			e.printStackTrace();
+			
+		}
 	}
 }
